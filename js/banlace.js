305bf21619bc92d15070ebfa813fcdf18df7ca9b
v-max07 <vusandebnath09@gmail.com>
2022-02-17 05:22:05 +0600

    error fixing

--- js/banlace.js
+++ js/banlace.js
@@ -16,13 +16,18 @@ function setSavingAmount() {
     //get income amount
     const incomeAmount = parseFloat(getInputValue('income_Input'));
 
+
     //get saving input value calling a function
     const savingLimit = parseFloat(getInputValue('saving_input'));
 
-    // set saving amount as a innerText
-    const savingAmount = incomeAmount * (savingLimit / 100);
+    if (savingLimit >= 0) {
+        // set saving amount as a innerText
+        const savingAmount = incomeAmount * (savingLimit / 100);
 
-    return savingAmount;
+        return savingAmount;
+    } else {
+        return 0;
+    }
 }
 
 // function for set remaining balance
@@ -31,11 +36,12 @@ function setRemaining() {
     const incomeAmount = parseFloat(getInputValue('income_Input'));
 
      //get total expenses 
-    const totalExpenses = parseFloat( getInnerText("totalExpenses"));
+    const totalExpenses = parseFloat(getInnerText("totalExpenses"));
+
     // set remaining Balance 
     const remainingBalance = incomeAmount - (totalExpenses + setSavingAmount());
 
-    document.getElementById('remaining_Balance').innerText = remainingBalance;
+    return remainingBalance;
 }
 
 
@@ -43,8 +49,8 @@ function setRemaining() {
 
 // add click event in Calculate Button
 document.getElementById('btnCalculate').addEventListener('click', function () {
+
     const incomeAmount = parseFloat(getInputValue('income_Input'));
-    console.log(incomeAmount)
     const foodExpenses = parseFloat(getInputValue('food_ExpensesInput'));
     const rentExpenses = parseFloat( getInputValue('rent_ExpensesInput'));
     const clothsExpenses = parseFloat(getInputValue('cloths_ExpensesInput'));
@@ -55,25 +61,41 @@ document.getElementById('btnCalculate').addEventListener('click', function () {
     // has money after saving 
     const moneyForExpenses = incomeAmount - savingAmount;
 
-
     // check incomeAmount, foodExpenses, rentExpenses and clothsExpenses are not smaller than 0
     // and also check the all type of expenses are lower than the actual monye 
 
-    if ((incomeAmount > 0) && (foodExpenses > 0 && moneyForExpenses > foodExpenses) && (rentExpenses > 0 &&moneyForExpenses > rentExpenses) && (clothsExpenses > 0 && moneyForExpenses>clothsExpenses)) {
-        // Change Total Expenses 
-        document.getElementById('totalExpenses').innerText = foodExpenses + rentExpenses + clothsExpenses;
+    if ((incomeAmount > 0) && (foodExpenses >= 0) && (moneyForExpenses >= foodExpenses) &&
+        (rentExpenses >= 0) && (moneyForExpenses >= rentExpenses) &&
+        (clothsExpenses >= 0) && (moneyForExpenses >= clothsExpenses)) {
+        
+        
+        // get total expenses
+        const expenses = foodExpenses + rentExpenses + clothsExpenses;
+        
+        if (expenses <= moneyForExpenses) {
+             // Change Total Expenses 
+            document.getElementById('totalExpenses').innerText = expenses;
 
-        const totalExpenses = parseFloat(getInnerText('totalExpenses'));
+            const totalExpenses = parseFloat(getInnerText('totalExpenses'));
 
-        //Balance after expenses
-        const subBalance = incomeAmount - totalExpenses;
-        document.getElementById('subBalance').innerText = subBalance;
+            //Balance after expenses
+            const subBalance = incomeAmount - totalExpenses;
+            document.getElementById('subBalance').innerText = subBalance;
 
-        //set remaining balance
-        setRemaining();
+            //set remaining balance
+            document.getElementById('remaining_Balance').innerText = setRemaining();
 
-        //hide alert
-        document.getElementById('alert').style.visibility = 'hidden';
+            //hide alert
+            document.getElementById('alert').style.visibility = 'hidden';
+
+        } else {
+            document.getElementById('alert').style.visibility = 'visible';
+
+            // after error reset the value 0
+            document.getElementById('totalExpenses').innerText = 0;
+            document.getElementById('subBalance').innerText = 0;
+            document.getElementById('remaining_Balance').innerText = 0;
+        }
     
     } else {
         document.getElementById('alert').style.visibility = 'visible';
@@ -81,6 +103,7 @@ document.getElementById('btnCalculate').addEventListener('click', function () {
         // after error reset the value 0
         document.getElementById('totalExpenses').innerText = 0;
         document.getElementById('subBalance').innerText = 0;
+        document.getElementById('remaining_Balance').innerText = 0;
     }
 
 
@@ -95,6 +118,6 @@ document.getElementById('save_Btn').addEventListener('click', function () {
     document.getElementById('savingAmount').innerText = setSavingAmount();
 
    //set remaining balance
-    setRemaining();
+    document.getElementById('remaining_Balance').innerText = setRemaining();
 
 })
